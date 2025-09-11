5a2e7719273ec1ec367c9c7a824cc553b504dd61
ordar80 <ordar80@29-7>
2025-09-11 10:13:08 +0300

    2132134124

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -9,6 +9,20 @@ private: // реализация объекта
     char s; // поле объекта
 
 public: // интерфейс объекта
+    Point()// конструктор поумолчанию
+    {
+        cout << "Constructor\n";
+        x = 0;
+        y = 0;
+        s = 'A';
+    }
+    Point(char z,int a,int b)
+    {
+        s = z;
+        x = a;
+        y = b;
+        cout << "Constructor by 3 params: \n";
+    }
     void Print() // метод класса 
     {
         cout << s << "-> X: " << x << "\tY: " << y << endl;
@@ -53,19 +67,26 @@ public: // интерфейс объекта
 
     }
 };
-int main() // клент
+int main() // вызов 
 {
-    Point a, b;
-    //cout << sizeof(a) << endl; // 12
-
+    Point a;// вызов конструкторов поумолчанию
+    a.Print();
     a.Init('A', 10, -20);
     a.Print();
-
-    b.Init('B', 5, 16);
+    Point b('B', 10, -20);//вызов конструктора с параметрами
     b.Print();
-    a.SetSumbol('T');
-    a.Print();
-    char c = a.GetSumbol();
-    cout << c << endl;
-    
+
+
+    //cout << sizeof(a) << endl; // 12
+
+    //a.Init('A', 10, -20);
+    //a.Print();
+
+    //b.Init('B', 5, 16);
+    //b.Print();
+    //a.SetSumbol('T');
+    //a.Print();
+    //char c = a.GetSumbol();
+    //cout << c << endl;
+    //
 }
