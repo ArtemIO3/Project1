866d905a9a31b24d14035b02924b7f64e2baad67
ordar80 <ordar80@29-7>
2025-09-15 11:37:32 +0300

    1312423535

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -4,26 +4,30 @@ using namespace std;
 class Student
 {
 	char *name;
+	char *surname;
 	int age;
 public:
-	Student(const char *n, int a)
+	Student(const char *n, const char *sur, int a)
 	{
+		surname = new char[strlen(sur) + 1];
+		strcpy_s(surname, strlen(sur)+1,sur);
 		name = new char[strlen(n) + 1];
 		strcpy_s(name, strlen(n) + 1, n);
 		age = a;
 	}
 	void Print()
 	{
-		cout << "Name:" << name << "\tAge:" << age << endl;
+		cout << "Name:" << name << "\tAge:" << age <<"\t Surname:" << surname  << endl;
 	}
 	~Student() // destructor 
 	{
 		cout << "destructor\n";
+		delete[]surname;
 		delete[] name; 
 	}
 };
 int main()
 {
-	Student obj1("Oleg", 19);
+	Student obj1("Artem","Iordan", 19);
 	obj1.Print();
 }
