fa6a0e62066cef0c14b08c047de336fd57a27b18
ordar80 <ordar80@29-7>
2025-09-15 10:44:07 +0300

    144235

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,92 +1,29 @@
 #include<iostream>
 using namespace std;
 
-class Point // создание своего ТИПА!!!
+class Student
 {
-private: // реализация объекта
-    int x; // поле объекта
-    int y; // поле объекта
-    char s; // поле объекта
-
-public: // интерфейс объекта
-    Point()// конструктор поумолчанию
-    {
-        cout << "Constructor\n";
-        x = 0;
-        y = 0;
-        s = 'A';
-    }
-    Point(char z,int a,int b)
-    {
-        s = z;
-        x = a;
-        y = b;
-        cout << "Constructor by 3 params: \n";
-    }
-    void Print() // метод класса 
-    {
-        cout << s << "-> X: " << x << "\tY: " << y << endl;
-    }
-    void Init(char t, int _x, int _y) // метод класса 
-    {
-        s = t;
-        x = _x;
-        y = _y;
-    }
-    // ----------------- методы аксессоры 
-    void SetSumbol(char sym)
-    {
-        s = sym;
-    }
-    char GetSumbol()
-    {
-        return s;
-    }
-    void SetX(int a)
-    {
-        if (a >= 0)
-        {
-            x = a;
-        }
-    }
-    int GetX()
-    {
-        return x;
-
-    }
-    void SetY(int a)
-    {
-        if (a >= 0)
-        {
-            y = a;
-        }
-    }
-    int GetY()
-    {
-        return y;
-
-    }
+	char *name;
+	int age;
+public:
+	Student(const char *n, int a)
+	{
+		name = new char[strlen(n) + 1];
+		strcpy_s(name, strlen(n) + 1, n);
+		age = a;
+	}
+	void Print()
+	{
+		cout << "Name:" << name << "\tAge:" << age << endl;
+	}
+	~Student() // destructor 
+	{
+		cout << "destructor\n";
+		delete[] name; 
+	}
 };
-int main() // вызов 
+int main()
 {
-    Point a;// вызов конструкторов поумолчанию
-    a.Print();
-    a.Init('A', 10, -20);
-    a.Print();
-    Point b('B', 10, -20);//вызов конструктора с параметрами
-    b.Print();
-
-
-    //cout << sizeof(a) << endl; // 12
-
-    //a.Init('A', 10, -20);
-    //a.Print();
-
-    //b.Init('B', 5, 16);
-    //b.Print();
-    //a.SetSumbol('T');
-    //a.Print();
-    //char c = a.GetSumbol();
-    //cout << c << endl;
-    //
+	Student obj1("Oleg", 19);
+	obj1.Print();
 }
